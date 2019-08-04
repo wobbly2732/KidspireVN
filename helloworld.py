40522e2298a7b51c3a07f7aa4c10af827f337742
Tim Chapman <12281550+wobbly2732@users.noreply.github.com>
2019-08-04 21:42:43 +0700

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,5 @@
 #!/bin/python3
-## Basic hello world script
-## Create by Tim Chapman (tim.chapman2732@gmail.com)
-## v1.0 dated 26th Jun 2019
+# -*- coding: utf-8 -*-
 
-print("xin chào")
-print("tên tôi là Tim")
+#Main
 
