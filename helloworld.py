79defa9b80be89fd9220d4a3ce77c49b8af6d6dc
Tim Chapman <12281550+wobbly2732@users.noreply.github.com>
2019-08-11 10:57:47 +0700

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,12 @@
 #!/bin/python3
 # -*- coding: utf-8 -*-
 
-#Main
+#Sample print statements
 print('Hello World')
 print()
-print('My name is Tim)
+print('My name is ....')
+print(3 + 2) #addition - 5
+print(5 - 3) #subtraction - 2
+print(5 * 5) #multiplication - 25
+print(30 / 3) #division - 10
+print(2 ** 3) #power of - 8
